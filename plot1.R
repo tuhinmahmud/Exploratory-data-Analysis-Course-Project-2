b56d29b526022a9f54134593c618807ec18702da
Tuhin Mahmud <tuhinm@hotmail.com>
2014-10-21 23:43:13 -0500

    plot1.png
    plot1.png

--- plot1.R
+++ plot1.R
@@ -1,9 +1,9 @@
 library(plyr)
-library(ggplot2)
+#library(ggplot2)
 
 aggregate.data <- with(pm25, aggregate(Emissions, by = list(year), sum))
 
-
+png("plot1.png", width=4, height=4, units="in", res=300)
 plot(aggregate.data, type = "o", ylab = expression("Total Emissions, PM"[2.5]), 
     xlab = "Year", main = "Total Emissions in the United States")
-    polygon(aggregate.data, col = "green", border = "blue")
+dev.off()
