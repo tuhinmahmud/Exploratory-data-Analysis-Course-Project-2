02dd8a2537743d519f9a434e4f60104f9c85733c
Tuhin Mahmud <tuhinm@hotmail.com>
2014-10-23 01:25:00 -0500

    plot6 update
    plot6 update

--- plot6.R
+++ plot6.R
@@ -1,46 +1,52 @@
-library(plyr)
-library(ggplot2)
+#Q6: Compare emissions from motor vehicle sources in Baltimore City 
+#      with emissions from motor vehicle sources in Los Angeles County, 
+#      California (fips == "06037"). 
+# Which city has seen greater changes over time in motor vehicle emissions?
 
+library(dplyr)
+library(ggplot2)
 pm25 <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-
+#get subset with Short name matchin "motor"
 SCC.motor <- grep("motor", SCC$Short.Name, ignore.case = TRUE)
 SCC.motor <- SCC[SCC.motor, ]
 SCC.identifiers <- as.character(SCC.motor$SCC)
 pm25$SCC <- as.character(pm25$SCC)
 pm25.motor <- pm25[pm25$SCC %in% SCC.identifiers, ]
-pm25.motor.24510 <- pm25.motor[which(pm25.motor$fips == "24510"), ]
-aggregate.motor.24510 <- with(pm25.motor.24510, aggregate(Emissions, by = list(year), sum))
 
-SCC.motor <- grep("motor", SCC$Short.Name, ignore.case = TRUE)
-SCC.motor <- SCC[SCC.motor, ]
-SCC.identifiers <- as.character(SCC.motor$SCC)
+#get the total Emission  data for Baltimore City
+pm25.motor.Baltimore <- pm25.motor[which(pm25.motor$fips == "24510"), ]
+
+adata.Baltimore<-
+    pm25.motor.Baltimore %>%
+    group_by(year) %>%
+    summarize(Emissions=sum(Emissions)) %>%
+    mutate(County = "Baltimore")
+
+#get the total Emission  data for Los Angles City
+pm25.motor.LosAngles <- pm25.motor[which(pm25.motor$fips == "06037"), ]
+adata.LosAngles <- 
+    pm25.motor.LosAngles %>%
+    group_by(year) %>%
+    summarize(Emissions=sum(Emissions)) %>%
+    mutate(County = "Los Angles")
+    
+
+#combine Baltimore and Los Angles data by rows
+adata.Combined <- rbind(adata.LosAngles, adata.Baltimore )
+adata.Combined$County <- as.factor(adata.Combined$County)
 
 
-pm25$SCC <- as.character(pm25$SCC)
-pm25.motor <- pm25[pm25$SCC %in% SCC.identifiers, ]
-
-pm25.motor.24510 <- pm25.motor[which(pm25.motor$fips == "24510"), ]
-pm25.motor.06037 <- pm25.motor[which(pm25.motor$fips == "06037"), ]
-
-aggregate.motor.24510 <- with(pm25.motor.24510, aggregate(Emissions, by = list(year), sum))
-aggregate.motor.24510$group <- rep("Baltimore County", length(aggregate.motor.24510[, 1]))
-
-aggregate.motor.06037 <- with(pm25.motor.06037, aggregate(Emissions, by = list(year), sum))
-aggregate.motor.06037$group <- rep("Los Angeles County", length(aggregate.motor.06037[, 1]))
-
-aggregated.motor.zips <- rbind(aggregate.motor.06037, aggregate.motor.24510)
-aggregated.motor.zips$group <- as.factor(aggregated.motor.zips$group)
-
-colnames(aggregated.motor.zips) <- c("Year", "Emissions", "Group")
-
-qplot(Year, Emissions,
-     data = aggregated.motor.zips,
-     group = Group,
-     color = Group, 
-     geom = c("point", "line"),
-     ylab = expression("Total Emissions, PM"[2.5]), 
-     xlab = "Year", 
-     main = "Comparison of Total Emissions by County"
-     )
+png("plot6.png")
+p<-qplot(year, Emissions,
+         data = adata.Combined,
+         group = County,
+         color = County,
+         geom = c("point", "line"),
+         ylab = expression("Total Emissions, PM"[2.5]),
+         xlab = "Year",
+         main = "Comparison of Total Emissions by County"
+)
+print(p)
+dev.off()
