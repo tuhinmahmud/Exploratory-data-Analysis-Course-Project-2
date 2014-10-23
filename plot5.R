163e1d78ba11a9cf401cc024119b284162990008
Tuhin Mahmud <tuhinm@hotmail.com>
2014-10-23 00:32:23 -0500

    plot5 update
    plot5 update

--- plot5.R
+++ plot5.R
@@ -1,18 +1,31 @@
+#Q5:How have emissions from motor vehicle sources changed from 
+#       1999-2008 in Baltimore City?
+
 library(ggplot2)
 library(plyr)
 pm25 <- readRDS("summarySCC_PM25.rds")
 scc <- readRDS("Source_Classification_Code.rds")
 
+#get subset data for name matching "motor"
 scc.motor <- grep("motor", scc$Short.Name, ignore.case = TRUE)
 scc.motor <- scc[scc.motor, ]
 scc.identifiers <- as.character(scc.motor$SCC)
 pm25$SCC <- as.character(pm25$SCC)
+
+#get subset data with valid identifier
 pm25.motor <- pm25[pm25$SCC %in% scc.identifiers, ]
 
-pm25.motor.24510 <- pm25.motor[which(pm25.motor$fips == "24510"), ]
+#get subset data for Baltimore City
+pm25.motor.Baltimore <- pm25.motor[which(pm25.motor$fips == "24510"), ]
 
-aggregate.motor.24510 <- with(pm25.motor.24510, aggregate(Emissions, by = list(year), 
-    sum))
+adata<-
+    pm25.motor.Baltimore %>%
+    group_by(year) %>%
+    summarize(Emissions=sum(Emissions)) 
 
-plot(aggregate.motor.24510, type = "o", ylab = expression("Total Emissions, PM"[2.5]), 
-        xlab = "Year", main = "Total Emissions from Motor Vehicle Sources")
+png("plot5.png")
+plot(adata, type = "o", 
+        ylab = expression("Total Emissions, PM"[2.5]), 
+        xlab = "Year",
+        main = "Total Emissions from Motor Vehicle Sources in Baltimore City")
+dev.off()
