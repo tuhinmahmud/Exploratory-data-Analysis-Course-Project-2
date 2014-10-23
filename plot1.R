88b1e27bd702590ba821a8d7400d9879c3f7659a
Tuhin Mahmud <tuhinm@hotmail.com>
2014-10-22 22:44:28 -0500

    last update for plot1.R
    last update for plot1.R and corresponding png file

--- plot1.R
+++ plot1.R
@@ -1,13 +1,17 @@
 library(dplyr)
+#load summarySSC_PM25.rds and Source_Classification_Code.rds
 source("load.R")
+#use dplyr to get the summary of aggregate data of Emission field
 adata<-
     pm25 %>%
     group_by(year) %>%
-    summarize(Esum=sum(Emissions))
+    summarize(total.emission=sum(Emissions))
 
+#plot the grap of year vs total emission
 png("plot1.png", width=4, height=4, units="in", res=300)
 plot(adata, type = "o", ylab = expression("Total Emissions, PM"[2.5]), 
     xlab = "Year", main = "Total Emissions in the United States")
-lmline <- lm(Esum~year,data=adata)
+#plot the regression line showing the decrease in the total emission over years
+lmline <- lm(total.emission~year,data=adata)
 abline(lmline,col="red")
 dev.off()
