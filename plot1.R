dc239a43e257819a85a81545db0fbfc77d869373
Tuhin Mahmud <tuhinm@hotmail.com>
2014-10-22 22:34:51 -0500

    plot1.png
    add lm line

--- plot1.R
+++ plot1.R
@@ -1,9 +1,13 @@
-library(plyr)
-#library(ggplot2)
-
-aggregate.data <- with(pm25, aggregate(Emissions, by = list(year), sum))
+library(dplyr)
+source("load.R")
+adata<-
+    pm25 %>%
+    group_by(year) %>%
+    summarize(Esum=sum(Emissions))
 
 png("plot1.png", width=4, height=4, units="in", res=300)
-plot(aggregate.data, type = "o", ylab = expression("Total Emissions, PM"[2.5]), 
+plot(adata, type = "o", ylab = expression("Total Emissions, PM"[2.5]), 
     xlab = "Year", main = "Total Emissions in the United States")
+lmline <- lm(Esum~year,data=adata)
+abline(lmline,col="red")
 dev.off()
