3898110ee49afb012804aad84c138f75df122505
Tuhin Mahmud <tuhinm@hotmail.com>
2014-10-23 00:00:27 -0500

    plot3 updated
    plot3 updated

--- plot3.R
+++ plot3.R
@@ -1,16 +1,28 @@
+#Q3.Of the four types of sources indicated by the type (point, nonpoint, onroad,
+#   nonroad) variable,
+#     i) which of these four sources have seen decreases in emissions from 1999-2008 
+#        for Baltimore City? 
+#     ii) Which have seen increases in emissions from 1999-2008? 
+library(dplyr)
 library(ggplot2)
-library(plyr)
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+#load summarySSC_PM25.rds and Source_Classification_Code.rds
+source("load.R")
 
-NEI.24510 <- NEI[which(NEI$fips == "24510"), ]
-aggregate.24510 <- with(NEI.24510, aggregate(Emissions, by = list(year), sum))
-colnames(aggregate.24510) <- c("year", "Emissions")
+#subset the data for baltimore city
+pm25baltimore <- pm25[which(pm25$fips == "24510"), ]
 
-NEI.24510.type <- ddply(NEI.24510, .(type, year), summarize, Emissions = sum(Emissions))
-NEI.24510.type$Pollutant_Type <- NEI.24510.type$type
+#use dplyr to get the summary of aggregate data of Emission field by
+#Type and year
+adataType<-
+    pm25baltimore %>%
+    group_by(type,year) %>%
+    summarize(Emissions=sum(Emissions)) 
 
-qplot(year, Emissions, data = NEI.24510.type, group = Pollutant_Type,
-      color = Pollutant_Type, geom = c("point", "line"),
+png("plot3.png")
+q<-qplot(year, Emissions, data = adataType, group = type,
+      color = type, geom = c("point", "line"),
       ylab = expression("Total Emissions, PM"[2.5]), 
       xlab = "Year", main = "Total Emissions in U.S. by Type of Pollutant")
+
+print(q)
+dev.off()
