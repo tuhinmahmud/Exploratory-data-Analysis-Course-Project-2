7962a17c5fac3844c1963065d5035c56b54a76d1
Tuhin Mahmud <tuhinm@hotmail.com>
2014-10-22 22:59:49 -0500

    update plot2 with linear regression lm line

--- plot2.R
+++ plot2.R
@@ -1,11 +1,27 @@
-pm25.24510 <- pm25[which(pm25$fips == "24510"), ]
-aggregate.24510 <- with(pm25.24510, 
-                        aggregate(Emissions, by = list(year), sum))
-colnames(aggregate.24510) <- c("year", "Emissions")
+#Q2: Have total emissions from PM2.5 decreased in the Baltimore City, 
+#Maryland (fips == "24510") from 1999 to 2008? 
 
+library(dplyr)
+#load summarySSC_PM25.rds and Source_Classification_Code.rds
+source("load.R")
 
-plot(aggregate.24510, type = "o",
+#get the subset of data where fips == 24510
+pm25subset <- pm25[which(pm25$fips == "24510"), ]
+
+#use dplyr to get the summary of aggregate data of Emission field
+adata<-
+    pm25subset %>%
+    group_by(year) %>%
+    summarize(Emissions=sum(Emissions))
+
+#plot the data 
+png("plot2.png", width=4, height=4, units="in", res=300)
+plot(adata, type = "o",
       ylab = expression("Total Emissions, PM"[2.5]), 
       xlab = "Year",
       main = "Total Emissions for Baltimore County",
       xlim = c(1999, 2008))
+#plot the regression line showing the decrease in the total emission over years
+lmline <- lm(Emissions~year,data=adata)
+abline(lmline,col="red")
+dev.off()
